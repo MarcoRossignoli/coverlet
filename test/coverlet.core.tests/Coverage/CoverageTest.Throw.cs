4cba919ecae63af5feb0bb7007ede32b28e67c56
Marco Rossignoli <marco.rossignoli@gmail.com>
2020-07-11 18:04:22 +0200

    try repro

---
+++ test/coverlet.core.tests/Coverage/CoverageTest.Throw.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Common;
+using System.IO;
+using System.Threading.Tasks;
+
+using Coverlet.Core.Samples.Tests;
+using Tmds.Utils;
+using Xunit;
+
+namespace Coverlet.Core.Tests
+{
+    public partial class CoverageTests : ExternalProcessExecutionTest
+    {
+        [Fact]
+        public void Throw_Issue890()
+        {
+            string path = Path.GetTempFileName();
+            try
+            {
+                FunctionExecutor.RunInProcess(async (string[] pathSerialize) =>
+                {
+                    CoveragePrepareResult coveragePrepareResult = await TestInstrumentationHelper.Run<Issue_890>(instance =>
+                    {
+                        instance.AddSkillRequired("value1");
+                        try
+                        {
+                            instance.AddSkillRequired("value");
+                        }
+                        catch { }
+                        return Task.CompletedTask;
+                    }, persistPrepareResultToFile: pathSerialize[0]);
+
+                    return 0;
+                }, new string[] { path });
+
+                CoverageResult result = TestInstrumentationHelper.GetCoverageResult(path);
+                result.GenerateReport(show: true);
+            }
+            finally
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
---
+++ test/coverlet.core.tests/Samples/Instrumentation.Throw.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coverlet.Core.Samples.Tests
+{
+    public partial class Issue_890
+    {
+        private List<string> _values = new List<string>() { "value" };
+
+        public void AddSkillRequired(string str)
+        {
+            if (_values.Any(v => v == str))
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
